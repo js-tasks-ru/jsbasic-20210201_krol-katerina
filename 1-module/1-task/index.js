65792a995de88d03b2073758c8d54fe5f6dbc595
Rayalt <krol.katerina@gmail.com>
2021-02-08 21:33:01 +0300

    task 1: added test for positive integer

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -5,8 +5,12 @@
  */
 function factorial(n) {
   let result = 1;
-  for (let i = n; i; i--) {
-    result *= i;
+  if (n > 0 && n % 1 === 0) {
+    for (let i = n; i; i--) {
+      result *= i;
+    }
+    return result;
+  } else {
+    return console.log('Введите целое неотрицательное число');
   }
-  return result;
 }
