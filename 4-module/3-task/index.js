6769e341c7a8b277ce9553fc6bb4431509ca6604
Rayalt <krol.katerina@gmail.com>
2021-02-25 14:38:56 +0300

    3 + 4 modules

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,41 @@
 function highlight(table) {
-  // ваш код...
+  let row = table.getElementsByTagName('tr');
+  let indexGender;
+  let indexAge;
+  let indexStatus;
+
+  for (let i = 0; i < row[0].children.length; i++) {
+    if (row[0].children[i].innerHTML === 'Age') {
+      indexAge = i;
+    }
+    if (row[0].children[i].innerHTML === 'Gender') {
+      indexGender = i;
+    }
+    if (row[0].children[i].innerHTML === 'Status') {
+      indexStatus = i;
+    }
+  };
+
+  let classes = {
+    m: 'male',
+    f: 'female',
+    true: 'available',
+    false: 'unavailable'
+  }
+
+  for (let m = 1; m < row.length; m++) {
+    let classGender = row[m].children[indexGender];
+    classGender.parentNode.classList.add(classes[classGender.innerHTML]);
+    let classAge = row[m].children[indexAge].innerHTML;
+    if (classAge < 18) {
+      classGender.parentNode.setAttribute('style', 'text-decoration: line-through');
+    }
+    let classStatus = row[m].children[indexStatus];
+    if (classStatus.hasAttribute('data-available')) {
+      let elem = classStatus.getAttribute('data-available');
+      classStatus.parentNode.classList.add(classes[elem]);
+    } else {
+      classStatus.parentNode.hidden = true;
+    }
+  }
 }
